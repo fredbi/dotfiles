086f8a66e52db34071892fdaccd2877d45712981
Frédéric BIDON <fredbi@yahoo.com>
2026-05-04 22:50:37 +0200

    update
    Signed-off-by: Frédéric BIDON <fredbi@yahoo.com>

--- .config/nvim/update.sh
+++ .config/nvim/update.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync';
-go install golang.org/x/tools/gopls@latest
-go install golang.org/x/tools/cmd/goimports@latest
+go install -a golang.org/x/tools/gopls@latest
+go install -a golang.org/x/tools/cmd/goimports@latest
